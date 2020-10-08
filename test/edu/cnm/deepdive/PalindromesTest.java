6cd60689aeea3d201405ca6201161a20e0411ce9
lsteiner9 <lsteiner1@cnm.edu>
2020-10-07 21:44:45 -0600

    Added test for Palindromes.testRecursive.

---
+++ test/edu/cnm/deepdive/PalindromesTest.java
@@ -0,0 +1,37 @@
+package edu.cnm.deepdive;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class PalindromesTest {
+
+
+  static final String[] inputValues = {
+      "radar",
+      "sonar",
+      "abba",
+      "abb",
+      "x",
+      ""
+  };
+
+  static final boolean[] expectedValues = {
+      true,
+      false,
+      true,
+      false,
+      true,
+      true
+  };
+
+  @Test
+  void testRecursive() {
+    for (int i = 0; i < inputValues.length - 1; i++) {
+      String input = inputValues[i];
+      boolean expected = expectedValues[i];
+      boolean actual = Palindromes.testRecursive(input);
+      assertEquals(expected, actual);
+    }
+  }
+}
